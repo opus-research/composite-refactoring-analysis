b230abaf7c54a956f1b676ebe613d431aafe573b
anacarlagb <ana.carlagb@gmail.com>
2021-05-23 17:56:31 -0300

    Get output of the 3rd, 4th, and 5th recommendations of catalog

--- src/main/java/opus/inf/puc/rio/composite/analysis/CompositeCatalogAnalyzer.java
+++ src/main/java/opus/inf/puc/rio/composite/analysis/CompositeCatalogAnalyzer.java
@@ -16,8 +16,11 @@ public class CompositeCatalogAnalyzer {
     public static void main(String[] args) {
 
         CompositeCatalogAnalyzer catalogAnalyzer = new CompositeCatalogAnalyzer();
-        //catalogAnalyzer.getCatalogFirstRecommendationResult();
-        catalogAnalyzer.getCatalogSecondRecommendationResult();
+        // catalogAnalyzer.getCatalogFirstRecommendationResult();
+        // catalogAnalyzer.getCatalogSecondRecommendationResult();
+        // catalogAnalyzer.getCatalogThirdRecommendationResult();
+        // catalogAnalyzer.getCatalogFourthRecommendationResult();
+        catalogAnalyzer.getCatalogFifthRecommendationResult();
     }
 
     private void getCatalogFirstRecommendationResult(){
@@ -112,5 +115,131 @@ public class CompositeCatalogAnalyzer {
         compositesWithTotalRemovalOfSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
         System.out.println("Total: " + compositesWithTotalRemovalOfSmells.size() + " composites");
 
+    }
+
+    private void getCatalogThirdRecommendationResult(){
+
+        List<CompositeGroup> summarizedGroups = groupAnalyzer.getCompositeGroupFromJson("summarized-groups.json");
+        System.out.println("RESULT OF THIRD RECOMMENDATION OF CATALOG");
+
+        List<String> groupNames = new ArrayList<>();
+        groupNames.add("[EXTRACT_METHOD]");
+        groupNames.add("[EXTRACT_METHOD, RENAME]");
+        groupNames.add("[EXTRACT_METHOD, REFACT_VARIABLE]");
+        groupNames.add("[EXTRACT_METHOD, REFACT_VARIABLE, RENAME]");
+        List<CompositeEffectDTO> composites = groupAnalyzer.gettAllCompositesFromSpecificGroups(groupNames, summarizedGroups);
+        System.out.println("Number of Composites: " + composites.size() + " composites");
+
+        List<String> existingSmells = new ArrayList<>();
+        existingSmells.add("LongMethod");
+        List<CompositeEffectDTO> compositesWithExistingSmells = effectAnalyzer.getCompositesThatHaveSpecificSmellTypes(existingSmells, composites);
+        System.out.println("Composites with Existing Smells: ");
+        compositesWithExistingSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithExistingSmells.size() + " composites");
+        List<CompositeEffectDTO> specificComposites = compositesWithExistingSmells;
+
+        //Composites with side effects
+        List<String> addedSmells = new ArrayList<>();
+        addedSmells.add("LongMethod");
+        List<CompositeEffectDTO> compositesWithAddedSmells = effectAnalyzer.getCompositesWithSmellTypesAddition(addedSmells, specificComposites);
+        System.out.println("Composites with Side Effects (Added Smells): ");
+        compositesWithAddedSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithAddedSmells.size() + " composites");
+
+        //Composites without side effects - composites that follow the recommendation
+        List<String> smellsTotallyRemoved = new ArrayList<>();
+        smellsTotallyRemoved.add("LongMethod");
+        List<CompositeEffectDTO> compositesWithTotalRemovalOfSmells = effectAnalyzer.getCompositesWithTotalSmellTypesRemoval(smellsTotallyRemoved, specificComposites);
+        System.out.println("Composites without Side Effects (Recommended Composites): ");
+        compositesWithTotalRemovalOfSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithTotalRemovalOfSmells.size() + " composites");
+
+    }
+
+    private void getCatalogFourthRecommendationResult(){
+
+        List<CompositeGroup> summarizedGroups = groupAnalyzer.getCompositeGroupFromJson("summarized-groups.json");
+        System.out.println("RESULT OF Fourth RECOMMENDATION OF CATALOG");
+
+        List<String> groupNames = new ArrayList<>();
+        groupNames.add("[EXTRACT_METHOD]");
+        groupNames.add("[EXTRACT_METHOD, RENAME]");
+        groupNames.add("[EXTRACT_METHOD, REFACT_VARIABLE]");
+        groupNames.add("[EXTRACT_METHOD, REFACT_VARIABLE, RENAME]");
+        List<CompositeEffectDTO> composites = groupAnalyzer.gettAllCompositesFromSpecificGroups(groupNames, summarizedGroups);
+        System.out.println("Number of Composites: " + composites.size() + " composites");
+
+        List<String> existingSmells = new ArrayList<>();
+        existingSmells.add("LongMethod");
+        List<CompositeEffectDTO> compositesWithExistingSmells = effectAnalyzer.getCompositesThatHaveSpecificSmellTypes(existingSmells, composites);
+        System.out.println("Composites with Existing Smells: ");
+        compositesWithExistingSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithExistingSmells.size() + " composites");
+
+        List<String> removedSmells = new ArrayList<>();
+        removedSmells.add("LongMethod");
+        List<CompositeEffectDTO> compositesWithRemovedSmells = effectAnalyzer.getCompositesWithSmellTypesRemoval(removedSmells, compositesWithExistingSmells);
+        System.out.println("Composites with Removed Smells: ");
+        compositesWithRemovedSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        List<CompositeEffectDTO> specificComposites = compositesWithRemovedSmells;
+        System.out.println("Total: " + specificComposites.size() + " composites");
+
+        //Composites with side effects
+        List<String> addedSmells = new ArrayList<>();
+        addedSmells.add("LongParameterList");
+        List<CompositeEffectDTO> compositesWithAddedSmells = effectAnalyzer.getCompositesWithSmellTypesAddition(addedSmells, specificComposites);
+        System.out.println("Composites with Side Effects (Added Smells): ");
+        compositesWithAddedSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithAddedSmells.size() + " composites");
+
+        //Composites without side effects - composites that follow the recommendation
+        List<String> smellsTotallyRemoved = new ArrayList<>();
+        smellsTotallyRemoved.add("LongParameterList");
+        List<CompositeEffectDTO> compositesWithTotalRemovalOfSmells = effectAnalyzer.getCompositesWithTotalSmellTypesRemoval(smellsTotallyRemoved, specificComposites);
+        System.out.println("Composites without Side Effects (Recommended Composites): ");
+        compositesWithTotalRemovalOfSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithTotalRemovalOfSmells.size() + " composites");
+
+    }
+
+
+    private void getCatalogFifthRecommendationResult(){
+
+        List<CompositeGroup> summarizedGroups = groupAnalyzer.getCompositeGroupFromJson("summarized-groups.json");
+        System.out.println("RESULT OF FIFTH RECOMMENDATION OF CATALOG");
+
+        List<String> groupNames = new ArrayList<>();
+        groupNames.add("[MOVE_METHOD]");
+        groupNames.add("[MOVE_METHOD, RENAME]");
+        groupNames.add("[MOVE_METHOD, REFACT_VARIABLE]");
+        groupNames.add("[MOVE_METHOD, REFACT_VARIABLE, RENAME]");
+        List<CompositeEffectDTO> composites = groupAnalyzer.gettAllCompositesFromSpecificGroups(groupNames, summarizedGroups);
+        System.out.println("Number of Composites: " + composites.size() + " composites");
+
+        List<String> existingSmells = new ArrayList<>();
+        existingSmells.add("FeatureEnvy");
+        List<CompositeEffectDTO> compositesWithExistingSmells = effectAnalyzer.getCompositesThatHaveSpecificSmellTypes(existingSmells, composites);
+        System.out.println("Composites with Existing Smells: ");
+        compositesWithExistingSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithExistingSmells.size() + " composites");
+
+        List<CompositeEffectDTO> specificComposites = compositesWithExistingSmells;
+        System.out.println("Total: " + specificComposites.size() + " composites");
+
+        //Composites with side effects
+        List<String> addedSmells = new ArrayList<>();
+        addedSmells.add("FeatureEnvy");
+        List<CompositeEffectDTO> compositesWithAddedSmells = effectAnalyzer.getCompositesWithSmellTypesAddition(addedSmells, specificComposites);
+        System.out.println("Composites with Side Effects (Added Smells): ");
+        compositesWithAddedSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithAddedSmells.size() + " composites");
+
+        //Composites without side effects - composites that follow the recommendation
+        List<String> smellsTotallyRemoved = new ArrayList<>();
+        smellsTotallyRemoved.add("FeatureEnvy");
+        List<CompositeEffectDTO> compositesWithTotalRemovalOfSmells = effectAnalyzer.getCompositesWithTotalSmellTypesRemoval(smellsTotallyRemoved, specificComposites);
+        System.out.println("Composites without Side Effects (Recommended Composites): ");
+        compositesWithTotalRemovalOfSmells.forEach(composite -> System.out.println(composite.getProject()+"-"+composite.getId()));
+        System.out.println("Total: " + compositesWithTotalRemovalOfSmells.size() + " composites");
     }
 }
